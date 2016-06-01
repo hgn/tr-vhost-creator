d9b03bb4715326afccae2288630dc1fe81418ddf
Hagen Paul Pfeifer <hagen@jauu.net>
2016-06-01 21:51:48 +0200

    install additional required modules
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- assets-lxc-host/post-install.sh
+++ assets-lxc-host/post-install.sh
@@ -9,3 +9,14 @@ sudo apt-get update
 sudo apt-get install aptitude
 sudo aptitude install git gitk
 sudo aptitude install python3-flask
+sudo aptitude install build-essential
+# flex & bison is required to build olsrd-v1
+sudo aptitude install bison flex
+# who knows, better already installed
+sudo aptitude install gdb
+
+git clone https://github.com/hgn/tr-olsrd-v1.git
+cd tr-olsrd-v1
+make build_all
+sudo make install
+
