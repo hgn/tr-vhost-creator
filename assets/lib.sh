80724a3fa778955621e3c4f6dce600e0e0ccf6f4
Hagen Paul Pfeifer <hagen@jauu.net>
2016-06-08 21:32:10 +0200

    restructure directory layout
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- assets/lib.sh
+++ assets/lib.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-function randomString {
+function  addBr() {
+	local brName=$1
+	local brDev=$2 || ""
+	if [ -d /sys/class/net/${brName} ]; then
+		# bridge exists
+		return
+	else
+		sudo ${BRCTL_BIN} addbr ${brName}
+		sudo ${BRCTL_BIN} setfd ${brName} 0
+		sudo ${BRCTL_BIN} sethello ${brName} 5
+		sudo ${IP_BIN} link set dev ${brName} up
+		if [ "${brDev}x" != "x" ]; then
+			sudo ${BRCTL_BIN} addif ${brName} ${brDev}
+			sudo ${IP_BIN} link set dev ${brDev} up
+		fi
+	fi
+}
+
+
+function randomString() {
 	if [[ -n $1 ]] && [[ "$1" -lt 20 ]]; then
 		local myStrLength=$1;
 	else
