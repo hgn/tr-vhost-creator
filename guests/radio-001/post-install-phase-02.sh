82d228dad4e0d2d1fc602b3510bf2b1bf8a516c8
Hagen Paul Pfeifer <hagen@jauu.net>
2016-06-08 22:52:14 +0200

    dont install X packages
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- guests/radio-001/post-install-phase-02.sh
+++ guests/radio-001/post-install-phase-02.sh
@@ -2,9 +2,12 @@
 
 cd /home/admin
 
+# Hey, beware: avoid to add packages with X dependencies!
+# No X on this machine!
+
 sudo apt-get update
 sudo apt-get -y install aptitude
-sudo aptitude --assume-yes -Z install git gitk
+sudo aptitude --assume-yes -Z install git
 sudo aptitude --assume-yes -Z install python3-flask
 sudo aptitude --assume-yes -Z install build-essential
 
