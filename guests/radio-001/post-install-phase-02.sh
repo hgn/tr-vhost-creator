52222be0ba49ff01e542890172c87710d10018b3
Hagen Paul Pfeifer <hagen@jauu.net>
2016-06-08 21:35:06 +0200

    format code
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- guests/radio-001/post-install-phase-02.sh
+++ guests/radio-001/post-install-phase-02.sh
@@ -7,7 +7,9 @@ sudo apt-get -y install aptitude
 sudo aptitude --assume-yes -Z install git gitk
 sudo aptitude --assume-yes -Z install python3-flask
 sudo aptitude --assume-yes -Z install build-essential
+
 # flex & bison is required to build olsrd-v1
 sudo aptitude --assume-yes -Z install bison flex
+
 # who knows, better already installed
 sudo aptitude --assume-yes -Z install gdb
