4eedc0a8f43e024d713a8020e7e8ca4ddd3ed88c
Hagen Paul Pfeifer <hagen.pfeifer@rohde-schwarz.com>
2016-06-16 11:41:04 +0200

    terminals don't run OLSR
    Signed-off-by: Hagen Paul Pfeifer <hagen.pfeifer@rohde-schwarz.com>

--- guests/terminals/shared/post-install-phase-03.sh
+++ guests/terminals/shared/post-install-phase-03.sh
@@ -12,11 +12,6 @@ cd $SOURCE_DIR
 git clone https://github.com/hgn/mcast-discovery-daemon.git
 git clone https://github.com/hgn/ipproof.git
 
-git clone https://github.com/hgn/tr-olsrd-v1.git
-cd tr-olsrd-v1
-make build_all
-sudo make install
-
 # script is executed via lxc-attach and thus the owner
 # must be corrected
 chown -R admin:admin /home/admin/$SOURCE_DIR
